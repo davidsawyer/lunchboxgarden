77ba6f71982b5ee136153d0e488088c12da671d3
Michael Crosby <microsby0@gmail.com>
2014-02-12 12:54:31 -0500

    Added room between officers

--- wp-content/themes/lunchboxgarden/page-about.php
+++ wp-content/themes/lunchboxgarden/page-about.php
@@ -10,8 +10,7 @@
             $loop = new WP_Query($args);
             while( $loop->have_posts()) : $loop->the_post();
         ?>
-            <!-- <p><?php the_field('position')?>: <?php the_field('name')?></p> -->
-            <div class="location_wrapper">
+            <div class="board_wrapper">
                 <img class="board_headshot" src="<?php echo the_field('board_member_image')?>"/>
                 <p class="board_info"><?php the_field('position')?></p>
                 <p class="board_info"><?php the_field('name')?></p>
