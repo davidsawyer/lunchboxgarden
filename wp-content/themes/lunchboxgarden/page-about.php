22ce9a92aa32b6ceda68afb78619a1e513dcc25a
David Sawyer <davidsawyer10@gmail.com>
2014-02-12 02:00:49 -0500

    give the about headings a little more breathing room

--- wp-content/themes/lunchboxgarden/page-about.php
+++ wp-content/themes/lunchboxgarden/page-about.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
     <div class="page_content">
-        <h2>Our Story</h2>
+        <h2 class="about_heading">Our Story</h2>
         <p><?php the_field('our_story') ?></p>
-        <h2>Our Mission</h2>
+        <h2 class="about_heading">Our Mission</h2>
         <p><?php the_field('our_mission') ?></p>
     </div>
 <?php get_footer(); ?>
