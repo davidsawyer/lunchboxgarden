33744101d3724ea27f2ea0c22f713e98d93b4ecf
Michael Crosby <microsby0@gmail.com>
2014-02-11 22:16:35 -0500

    Finished Where We Are page. Possibly force address into two lines

--- wp-content/themes/lunchboxgarden/page-where-we-are.php
+++ wp-content/themes/lunchboxgarden/page-where-we-are.php
@@ -1,2 +1,15 @@
 <?php get_header(); ?>
+    <div class="page_content">
+        <?php
+            $args = array('post_type' => 'location');
+            $loop = new WP_Query($args);
+            while( $loop->have_posts()) : $loop->the_post();
+        ?>
+            <div>
+                <h2><?php the_field('location_name') ?></h2>
+                <img class="location_img" src="<?php echo the_field('location_image')?>"/>
+                <p class="location_addr"><?php the_field('location_address') ?></p>
+            </div>
+        <?php endwhile; ?>
+    </div>
 <?php get_footer(); ?>
