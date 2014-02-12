63dadcadfcc07418e6d701654100f5ee2a69c86e
Michael Crosby <microsby0@gmail.com>
2014-02-12 09:50:34 -0500

    Added officers to about page. Used similar setup as locations, css classes may need to be changed as they currently say location wrapper. Left option to just have text commented in page-about

--- wp-content/themes/lunchboxgarden/page-about.php
+++ wp-content/themes/lunchboxgarden/page-about.php
@@ -4,5 +4,18 @@
         <p><?php the_field('our_story') ?></p>
         <h2 class="about_heading">Our Mission</h2>
         <p><?php the_field('our_mission') ?></p>
+        <h2 class="about_heading">Our Leaders</h2>
+        <?php
+            $args = array('post_type' => 'board_member');
+            $loop = new WP_Query($args);
+            while( $loop->have_posts()) : $loop->the_post();
+        ?>
+            <!-- <p><?php the_field('position')?>: <?php the_field('name')?></p> -->
+            <div class="location_wrapper">
+                <img class="board_headshot" src="<?php echo the_field('board_member_image')?>"/>
+                <p class="board_info"><?php the_field('position')?></p>
+                <p class="board_info"><?php the_field('name')?></p>
+            </div>
+        <? endwhile; ?>
     </div>
 <?php get_footer(); ?>
