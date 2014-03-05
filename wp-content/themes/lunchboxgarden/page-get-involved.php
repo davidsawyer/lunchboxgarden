6641241390c48ee9aca7d4d855b5e4079ba21287
Michael Crosby <microsby0@gmail.com>
2014-03-05 14:31:17 -0500

    Added styled form

--- wp-content/themes/lunchboxgarden/page-get-involved.php
+++ wp-content/themes/lunchboxgarden/page-get-involved.php
@@ -1,2 +1,73 @@
 <?php get_header(); ?>
+<h2>Join the Lunchbox!</h2>
+<form action="https://docs.google.com/forms/d/1lN6RY5wW2eLIbfkgFh5eYpHseXQwQfI3mHUC2hoVJbU/formResponse" method="POST" id="ss-form" target="_self" onsubmit=""><div role="list">
+<div class="ss-form-question errorbox-good" role="listitem">
+<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_511236397"><div class="ss-q-title">Last Name
+<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+<span class="ss-required-asterisk">*</span></div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<input type="text" name="entry.511236397" value="" class="ss-q-short" id="entry_511236397" dir="auto" aria-label="Last Name  " aria-required="true" required="" title="">
+<div class="error-message"></div>
+
+</div></div></div> <div class="ss-form-question errorbox-good" role="listitem">
+<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_1476588221"><div class="ss-q-title">First Name
+<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+<span class="ss-required-asterisk">*</span></div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<input type="text" name="entry.1476588221" value="" class="ss-q-short" id="entry_1476588221" dir="auto" aria-label="First Name  " aria-required="true" required="" title="">
+<div class="error-message"></div>
+
+</div></div></div> <div class="ss-form-question errorbox-good" role="listitem">
+<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_1718995006"><div class="ss-q-title">Email Address
+<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+<span class="ss-required-asterisk">*</span></div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<input type="text" name="entry.1718995006" value="" class="ss-q-short" id="entry_1718995006" dir="auto" aria-label="Email Address  " aria-required="true" required="" title="">
+<div class="error-message"></div>
+
+</div></div></div> <div class="ss-form-question errorbox-good" role="listitem">
+<div dir="ltr" class="ss-item ss-item-required ss-radio"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_1684420082"><div class="ss-q-title">Would you like to volunteer to help?
+<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+<span class="ss-required-asterisk">*</span></div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+
+<ul class="ss-choices" role="radiogroup" aria-label="Would you like to volunteer to help?  "><li class="ss-choice-item"><label><span class="ss-choice-item-control goog-inline-block"><input type="radio" name="entry.1672092475" value="Yes" id="group_1672092475_1" role="radio" class="ss-q-radio" aria-label="Yes" required="" aria-required="true"></span>
+<span class="ss-choice-label">Yes</span>
+</label></li> <li class="ss-choice-item"><label><span class="ss-choice-item-control goog-inline-block"><input type="radio" name="entry.1672092475" value="No" id="group_1672092475_2" role="radio" class="ss-q-radio" aria-label="No" required="" aria-required="true"></span>
+<span class="ss-choice-label">No</span>
+</label></li></ul>
+<div class="error-message"></div>
+</div></div></div> <div class="ss-form-question errorbox-good" role="listitem">
+<div dir="ltr" class="ss-item ss-item-required ss-checkbox"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_1712085380"><div class="ss-q-title">If so, what school(s) would you prefer to work with?
+<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+<span class="ss-required-asterisk">*</span></div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+
+<ul class="ss-choices ss-choices-required" role="group" aria-label="If so, what school(s) would you prefer to work with?  "><li class="ss-choice-item"><label><span class="ss-choice-item-control goog-inline-block"><input type="checkbox" name="entry.461311624" value="Barnett Shoals Elementary School&nbsp;" id="group_461311624_1" role="checkbox" class="ss-q-checkbox" aria-required="true"></span>
+<span class="ss-choice-label">Barnett Shoals Elementary School </span>
+</label></li> <li class="ss-choice-item"><label><span class="ss-choice-item-control goog-inline-block"><input type="checkbox" name="entry.461311624" value="Chase Street Elementary School&nbsp;" id="group_461311624_2" role="checkbox" class="ss-q-checkbox" aria-required="true"></span>
+<span class="ss-choice-label">Chase Street Elementary School </span>
+</label></li> <li class="ss-choice-item"><label><span class="ss-choice-item-control goog-inline-block"><input type="checkbox" name="entry.461311624" value="__other_option__" id="group_461311624_3" role="checkbox" class="ss-q-checkbox ss-q-other-toggle" aria-required="true"></span>
+Other:</label>
+<span class="ss-q-other-container goog-inline-block"><input type="text" name="entry.461311624.other_option_response" value="" class="ss-q-other" id="entry_461311624_other_option_response" dir="auto" aria-label="Other"></span>
+</li></ul>
+<div class="error-message"></div>
+</div></div></div> <div class="ss-form-question errorbox-good" role="listitem">
+<div dir="ltr" class="ss-item  ss-paragraph-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_2070155144"><div class="ss-q-title">Comments/Questions
+</div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<textarea name="entry.2070155144" rows="8" cols="43" class="ss-q-long" id="entry_2070155144" dir="auto" aria-label="Comments/Questions  "></textarea>
+<div class="error-message"></div>
+
+</div></div></div>
+<input type="hidden" name="draftResponse" value="[,,&quot;-8289000268093614968&quot;]
+">
+<input type="hidden" name="pageHistory" value="0">
+<input type="hidden" name="fromEmail" value="false">
+
+<input type="hidden" name="fbzx" value="-8289000268093614968">
+
+<div class="ss-item ss-navigate"><table id="navigation-table"><tbody><tr><td class="ss-form-entry goog-inline-block" id="navigation-buttons" dir="ltr">
+<input type="submit" name="submit" value="Submit" id="ss-submit">
+</tr></tbody></table></div></div></form>
 <?php get_footer(); ?>
